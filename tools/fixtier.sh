4a10e2832a58bec736dd7536c72a83a06c309563
KFL <kludev@gmail.com>
2018-06-28 17:37:15 -0700

    .

---
+++ tools/fixtier.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# fixtier.sh <blob_path>
+# This script is a wrapper of fixtier.exe. It should be
+# placed in the same directory.
+
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+SCRIPT="$DIR/fixtier.exe"
+MONO="/usr/local/bin/mono"
+
+mkdir -p /tmp/fixtier
+
+printf "`date` -------------------\n" >> /tmp/fixtier/log
+$MONO $SCRIPT \
+    --connection-string='_________________' \
+    --container=backup \
+    --blob-path="$1" 2>&1 >> /tmp/fixtier/log
+
+ret=$?
+if [ $ret -ne 0 ]; then
+    printf "`date`,$1\n" >> /tmp/fixtier/error.csv
+fi
+
+exit $ret
+
